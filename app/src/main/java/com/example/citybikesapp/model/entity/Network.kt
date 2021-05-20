ba9da8cc645ad8576253bcda77b0af6f6edc08ca
Daniel Wojdak <logiblogi@gmail.com>
2021-05-20 16:32:42 +0200

    Fix company type bug

--- app/src/main/java/com/example/citybikesapp/model/entity/Network.kt
+++ app/src/main/java/com/example/citybikesapp/model/entity/Network.kt
@@ -1,8 +1,19 @@
 package com.example.citybikesapp.model.entity
 
+import com.google.gson.annotations.SerializedName
+
 data class Network(
-    val company: List<String>,
+    @SerializedName("company")
+    private val any: Any,
     val location: Location,
     val name: String,
     val id: String
-)
+) {
+    val company: List<String>
+    get() =
+        if(any is List<*>)
+            if(any.firstOrNull() is String) any as List<String>
+            else emptyList()
+        else if(any is String) listOf(any.toString())
+        else emptyList()
+}
